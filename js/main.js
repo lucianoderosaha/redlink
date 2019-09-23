0f6ae3f3880e917a8c349377d9ca5cdf09e5354b
lucianoderosaha <45297835+lucianoderosaha@users.noreply.github.com>
2019-09-23 13:59:09 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,272 +4,238 @@ $(document).ready(function() {
 					name:"Banco Provincia",
 					links:{
 						web: "https://www.bancoprovincia.com.ar/Principal/BipPersonal",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco hipotecario",
 					links:{
 						web: "https://hb.hipotecario.com.ar/hb/",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Credicoop",
 					links:{
 						web: "https://bancainternet.bancocredicoop.coop/bcclbi/",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Bica",
 					links:{
 						web: "https://hb.redlink.com.ar/bancobica/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Coinag",
 					links:{
 						web: "https://hb.redlink.com.ar/bancocoinag/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Ciudad de Buenos Aires",
 					links:{
 						web: "https://hb.redlink.com.ar/ciudad/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco de Comercio",
 					links:{
 						web: "https://hb.redlink.com.ar/bancodecomercio/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco de Córdoba",
 					links:{
 						web: "https://hb.redlink.com.ar/bancor/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco de Corrientes",
 					links:{
 						web: "https://hb.redlink.com.ar/bancodecorrientes/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco de la Nación Argentina",
 					links:{
 						web: "https://hb.redlink.com.ar/bna/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco de La Pampa",
 					links:{
 						web: "https://hb.redlink.com.ar/bancodelapampa/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Provincia del Neuquen",
 					links:{
 						web: "https://hb.redlink.com.ar/bpn/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco de Tierra del Fuego",
 					links:{
 						web: "https://hb.redlink.com.ar/btf/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco del Chubut",
 					links:{
 						web: "https://hb.redlink.com.ar/bancochubut/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Entre Ríos",
 					links:{
 						web: "https://hb.redlink.com.ar/bancoentrerios/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Formosa",
 					links:{
 						web: "https://hb.redlink.com.ar/bancodeformosa/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Industrial",
 					links:{
 						web: "https://www.bind.com.ar/banca-electronica",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Mariva",
 					links:{
 						web: "https://hb.redlink.com.ar/mariva/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Masventas",
 					links:{
 						web: "https://hb.redlink.com.ar/bancomasventas/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Meridian",
 					links:{
 						web: "https://hb.redlink.com.ar/bancomeridian/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Municipal",
 					links:{
 						web: "https://hb.redlink.com.ar/bmros/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Piano",
 					links:{
 						web: "https://hb.redlink.com.ar/bancopiano/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Rioja",
 					links:{
 						web: "https://hb.redlink.com.ar/nblr/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Roela",
 					links:{
 						web: "https://hb.redlink.com.ar/bancoroela/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Saenz",
 					links:{
 						web: "https://hb.redlink.com.ar/bancosaenz/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco San Juan",
 					links:{
 						web: "https://hb.redlink.com.ar/bsj/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Santa Cruz",
 					links:{
 						web: "https://hb.redlink.com.ar/bancosantacruz/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Santa Fé",
 					links:{
 						web: "https://hb.redlink.com.ar/bancobsf/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Banco Santiago del Estero",
 					links:{
 						web: "https://hb.redlink.com.ar/bse/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Caja de Crédito Cuenca",
 					links:{
 						web: "https://hb.redlink.com.ar/cccuenca/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Consumax",
 					links:{
 						web: "https://hb.redlink.com.ar/consumax/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Finandino Cía. Financiera",
 					links:{
 						web: "https://hb.redlink.com.ar/finandino/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Italcred Cía. Financiera",
 					links:{
 						web: "https://hb.redlink.com.ar/italcred/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 				{
 					name:"Nuevo Banco del Chaco",
 					links:{
 						web: "https://hb.redlink.com.ar/nbch/login.htm",
-						ios: "",
-						android: ""
+						app: ""
 					}
 				},
 	];
@@ -293,16 +259,12 @@ $(document).ready(function() {
 				$(web).html("Sitio Web");
 				$(".como").append(web);
 
-				if(bancos[bancoIndex].links.ios != ""){
-					var ios = new Option("App Store Apple",  bancos[bancoIndex].links.ios);
-					$(ios).html("App Store Apple");
-					$(".como").append(ios);
+				if(bancos[bancoIndex].links.app != ""){
+					var app = new Option("App Móvil",  bancos[bancoIndex].links.app);
+					$(app).html("App Móvil");
+					$(".como").append(app);
 				 }
-				if(bancos[bancoIndex].links.android != ""){
-					var android = new Option("Play Store Android",  bancos[bancoIndex].links.android);
-					$(android).html("Play Store Android");
-					$(".como").append(android);
-				 }
+			
 
 			$(".how-select").show();	
 			$("#navigateBtn").hide();
